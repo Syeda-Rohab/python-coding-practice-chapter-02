0f61318371380ba1912f93498e6e636c518babc3
learn typescript  with sr <syedarohab8@gmail.com>
2025-05-29 20:05:45 +0500

    Add files via upload

---
+++ chap2.py
@@ -0,0 +1,90 @@
+# chap = 02 python 
+# topics : variables , data types , operators
+# variables , integers , floats , strings , booleans , arthematic and logical operators
+# topic 1
+# Q1 ) variables kiya hain ?
+# A )variables aese containers hote h jisme ham data ko store krt hain jese koi dabba jisme ham kuch rakh sakte h wese hi variables m ham numbers , text , ya koi aur data rakh sakte h variable ko aik naam diya jata h jisse ham us data ko program m use krskte h .
+
+# topic 2
+# Q2 ) Data types kiya h ?
+# A ) har variable m jo data hota hai , uski aik type hoti h python m common data types ye hain :
+# 1 ) int = (integer) : poore number jese (5 , 100 , -3)
+# 2 ) float : decimal wale numbers jese (3.14 , -0.5)
+# 3 ) str = (string) : text jese "" double comma ko python m string kehte h (example : "hello" , "python")
+# 4 ) bool = (boolean) : sirf do value leskte h (true or false)
+# 5 ) complex : jese 1+2j python automatically data type samajh leta h jab ap value assign krte h
+
+
+# topic 3
+# Q1 )operators kiya h ?
+# A ) operators symbol hote h jo variables ke value par operations krte h kuch common operators :
+# 1 ) addition symbol (+)
+# 2 ) subtraction symbol (-)
+# 3 ) multiplication symbol (*)
+# 4 ) division symbol (/)
+# 5 ) modulus , remainder symbol (%)
+# 6 ) equals to symbol (==)
+#  7) less than , greater than (less than symbol = < ) , (greater than symbol = >)
+# ye operators numbers ya string par kaam krte h 
+
+
+# variables aur data types ki example coding base
+
+x = 10
+print(x)
+print(type(x)) # this code is integer 
+
+y = 3.14
+print (y)
+print(type(y)) # this code is float
+
+name = "Syeda Rohab"
+print(name)
+print(type(name)) # this code is string
+
+is_student = True
+print(is_student)
+print(type(is_student)) # this code is boolean
+
+
+# operators example
+
+a = 5 
+b = 5
+print(a + b)
+print(type(a + b)) # this code is addition
+
+c = 5 
+d = 5
+print( c - d)
+print(type(c - d)) # this code is subtraction
+
+e = 5 
+f = 5
+print( e * f)
+print(type(e * f)) # this code is multiply
+
+g = 5 
+h = 5
+print(g / h)
+print(type(g / h)) # this code is divide 
+
+i = 5 
+j = 20
+print(i % j)
+print(type(g % h)) # this code is modulus
+
+k = 5 
+l = 5
+print(k == l)
+print(type(k == l)) # this code is equal to
+
+m = 5 
+n = 10
+print(m < n)
+print(type(m < n)) # this code is less than
+
+o = 5 
+p = 10
+print(o > p)
+print(type(o > p)) # this code is greater than
